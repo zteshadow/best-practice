75fb505d46c23e842b8b5a905a680c8b4ea1708e
Samuel Song <samuel.song@agoda.com>
2023-05-01 09:53:17 +0700

    add A at branch a

---
+++ native-ios/MergeConflict/MCSample/MCSample/SwiftUIViewA.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIViewA.swift
+//  MCSample
+//
+//  Created by Samuel Song on 5/1/66.
+//
+
+import SwiftUI
+
+struct SwiftUIViewA: View {
+    var body: some View {
+        Text("This is A view")
+    }
+}
+
+struct SwiftUIViewA_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIViewA()
+    }
+}
