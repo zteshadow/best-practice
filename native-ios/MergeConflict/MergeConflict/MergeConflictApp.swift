e20ea14d97685d2d560330a4d4ea395a4b874189
Samuel Song <samuel.song@agoda.com>
2023-05-01 10:28:44 +0700

    restart

---
+++ native-ios/MergeConflict/MergeConflict/MergeConflictApp.swift
@@ -0,0 +1,17 @@
+//
+//  MergeConflictApp.swift
+//  MergeConflict
+//
+//  Created by Samuel Song on 5/1/66.
+//
+
+import SwiftUI
+
+@main
+struct MergeConflictApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
