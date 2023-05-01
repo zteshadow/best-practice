5033784cb01f3a065884094d7f5c90b4abd5b06a
Samuel Song <samuel.song@agoda.com>
2023-05-01 09:50:33 +0700

    init

---
+++ native-ios/MergeConflict/MCSample/MCSample/MCSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MCSampleApp.swift
+//  MCSample
+//
+//  Created by Samuel Song on 5/1/66.
+//
+
+import SwiftUI
+
+@main
+struct MCSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
