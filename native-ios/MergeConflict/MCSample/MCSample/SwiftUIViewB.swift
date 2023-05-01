73458eef2dd024b35e06b6af8b7c9776db5e154e
Samuel Song <samuel.song@agoda.com>
2023-05-01 09:51:40 +0700

    add B

---
+++ native-ios/MergeConflict/MCSample/MCSample/SwiftUIViewB.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIViewB.swift
+//  MCSample
+//
+//  Created by Samuel Song on 5/1/66.
+//
+
+import SwiftUI
+
+struct SwiftUIViewB: View {
+    var body: some View {
+        Text("This is B view")
+    }
+}
+
+struct SwiftUIViewB_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIViewB()
+    }
+}
