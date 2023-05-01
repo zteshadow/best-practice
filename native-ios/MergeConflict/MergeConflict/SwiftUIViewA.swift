6c7dda69e014d6afb9795ba25793833776b61a76
Samuel Song <samuel.song@agoda.com>
2023-05-01 10:31:29 +0700

    add A view

---
+++ native-ios/MergeConflict/MergeConflict/SwiftUIViewA.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIViewA.swift
+//  MergeConflict
+//
+//  Created by Samuel Song on 5/1/66.
+//
+
+import SwiftUI
+
+struct SwiftUIViewA: View {
+    var body: some View {
+        Text("This is A view at a branch")
+    }
+}
+
+struct SwiftUIViewA_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIViewA()
+    }
+}
